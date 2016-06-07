bf7375ca0e901de85ab18197e7434c53a814e0f8
yukiohnuki <yukiohnuki@yuki-no-MacBook-Pro.local>
2016-06-07 17:24:28 +0900

    Ruby入門20~26

--- hello.rb
+++ hello.rb
@@ -1,13 +1,23 @@
-class User
-	def initialize(name)
-     @name = name
-	end
+#例外
 
-	def sayHi
-		puts "hi! i am #{@name}"
-	end
+class MyError < StandardError; end
 
+x = gets.to_i
+
+begin
+  if x == 3
+  	raise MyError
+  end
+  
+  p 100 / x
+  
+  rescue MyError
+   puts "not 3!"
+
+rescue => ex
+	p ex.message
+	p ex.class
+	puts "stopped!"
+ensure
+	puts "-- END --"
 end
-
-tom = User.new("tom")
-tom.sayHi
