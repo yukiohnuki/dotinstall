64d7f94b9886b7ce97f101f60659fc3b42b9a345
yukiohnuki <yukiohnuki@yuki-no-MacBook-Pro.local>
2016-06-07 22:24:27 +0900

    Rails入門1~15

---
+++ taskapp/app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < ApplicationController
+
+	def index
+		@projects = Project.all
+    end
+
+    def show
+    	@project = Project.find(params[:id])
+    end
+
+    def new
+    	@project = Project.new
+    end
+
+    def create
+    	@project = Project.new(project_params)
+    	if @project.save
+    	   redirect_to projects_path
+        else
+        	render 'new'
+        end
+    end
+
+    private
+
+     def project_params
+     	 params[:project].permit(:title)
+     end
+
+end
---
+++ taskapp/app/models/project.rb
@@ -0,0 +1,5 @@
+class Project < ActiveRecord::Base
+	validates :title,
+	presence: { message: "入力してください"},
+	length: { minimum:3, message: "短すぎ!"}
+end
