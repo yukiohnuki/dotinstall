da8fc81af7092ae7892ff7fdb9290274dce942f8
yukiohnuki <yukiohnuki@yuki-no-MacBook-Pro.local>
2016-06-08 10:58:37 +0900

    ドットインストールruby on rails入門16~18

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -21,6 +21,25 @@ class ProjectsController < ApplicationController
         end
     end
 
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(project_params)
+            redirect_to projects_path
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+      @project = Project.find(params[:id])
+      @project.destroy
+      redirect_to projects_path
+    end
+
     private
 
      def project_params
